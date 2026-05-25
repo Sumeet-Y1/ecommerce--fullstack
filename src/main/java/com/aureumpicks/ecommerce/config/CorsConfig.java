4ec0b1bc7730fd62baec905366fd18e1213784b3
Sumeet-Y1 <sumeety202@gmail.com>
2026-05-25 14:31:59 +0530

    added link in cors

--- src/main/java/com/aureumpicks/ecommerce/config/CorsConfig.java
+++ src/main/java/com/aureumpicks/ecommerce/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
         configuration.setAllowCredentials(true);
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://aureumpicks.pages.dev"
                 // Add the deployed Cloudflare Pages origin here when ready.
                 // "https://aureumpicks.pages.dev",
                 // "https://aureumpicks.com"
